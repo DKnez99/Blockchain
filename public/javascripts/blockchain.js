d8493f56ae21ad8492a840e6a507776d64bf95d8
DKnez99 <dknez1999@gmail.com>
2022-04-10 23:29:11 +0200

    added basic errors to blockchain

--- public/javascripts/blockchain.js
+++ public/javascripts/blockchain.js
@@ -1,18 +1,34 @@
-// function updateState(block, chain) {
-//     // set the well background red or green for this block
-//     if ($('#block'+block+'chain'+chain+'hash').val().substr(0, difficulty) === pattern) {
-//       $('#block'+block+'chain'+chain+'well').removeClass('well-error').addClass('well-success');
-//     }
-//     else {
-//       $('#block'+block+'chain'+chain+'well').removeClass('well-success').addClass('well-error');
-//     }
-//   }
+function changeDetected(chain, block) {
+    // set the card background
+    $('#chain'+chain+'block'+block+'card').removeClass('bg-light');
+    $('#chain'+chain+'block'+block+'card').addClass('card-change-detected');
+    //$('#chain'+chain+'block'+block+'card').removeClass('card-change-detected').addClass('bg-light');
+    //$('#chain'+chain+'block'+block+'card').removeClass('card-change-detected');
+  }
   
   function updateHash(chain, block) {
     $('#chain'+chain+'block'+block+'hash').val(sha256(chain, block));
-    //updateState(block, chain);
+    
   }
   
+  function updateBlockchain(chain, block, chainSize) {  //ADD BLOCKID AND MAGIC CHECK
+    updateHash(chain, block);
+    for (var i = 1; i <= chainSize; i++) {
+      if(($('#chain'+chain+'block'+i+'hash').val()!=$('#chain'+chain+'block'+(i+1).toString()+'prev').val() && i!=chainSize) || $('#chain'+chain+'block'+(i-1).toString()+'card').hasClass('card-error')){
+        $('#chain'+chain+'block'+i+'card').removeClass('bg-light').addClass('card-error');
+        if($('#chain'+chain+'block'+i+'hash').val()!=$('#chain'+chain+'block'+(i+1).toString()+'prev').val() && i!=chainSize){
+          $('#chain'+chain+'block'+i+'hash').addClass('input-error');
+          $('#chain'+chain+'block'+(i+1).toString()+'prev').addClass('input-error');
+        }
+      }
+      else{
+        $('#chain'+chain+'block'+i+'card').removeClass('card-error').addClass('bg-light');
+        $('#chain'+chain+'block'+i+'hash').removeClass('input-error');
+        $('#chain'+chain+'block'+(i+1).toString()+'prev').removeClass('input-error');
+      }
+    }
+  }
+
   function updateChain(chain, block, chainSize) {
     for (var i = block; i <= chainSize; i++) {
       if (i > 1) {
