3f9e052ca6eaac10e6dac547ea38f34a36866c13
DKnez99 <dknez1999@gmail.com>
2022-04-11 15:29:17 +0200

    fixed magic and blockid errors in blockchain tab

--- public/javascripts/blockchain.js
+++ public/javascripts/blockchain.js
@@ -1,39 +1,70 @@
-function changeDetected(chain, block) {
-    // set the card background
-    $('#chain'+chain+'block'+block+'card').removeClass('bg-light');
-    $('#chain'+chain+'block'+block+'card').addClass('card-change-detected');
-    //$('#chain'+chain+'block'+block+'card').removeClass('card-change-detected').addClass('bg-light');
-    //$('#chain'+chain+'block'+block+'card').removeClass('card-change-detected');
-  }
-  
-  function updateHash(chain, block) {
-    $('#chain'+chain+'block'+block+'hash').val(sha256(chain, block));
-    
-  }
-  
-  function updateBlockchain(chain, block, chainSize) {  //ADD BLOCKID AND MAGIC CHECK
-    updateHash(chain, block);
-    for (var i = 1; i <= chainSize; i++) {
-      if(($('#chain'+chain+'block'+i+'hash').val()!=$('#chain'+chain+'block'+(i+1).toString()+'prev').val() && i!=chainSize) || $('#chain'+chain+'block'+(i-1).toString()+'card').hasClass('card-error')){
+function updateHash(chain, block) {
+  $('#chain'+chain+'block'+block+'hash').val(sha256(chain, block)); 
+}
+
+function updateBlockchain(chain, block, chainSize) {
+  updateHash(chain, block);
+
+  for(var i = 2; i<=chainSize; i++){
+    if($('#chain'+chain+'block'+(i-1).toString()+'magic').val()!=$('#chain'+chain+'block'+i+'magic').val() ||
+      parseInt($('#chain'+chain+'block'+(i-1).toString()+'blockid').val())!=parseInt($('#chain'+chain+'block'+i+'blockid').val())-1 ||
+      $('#chain'+chain+'block'+(i-1).toString()+'hash').val()!=$('#chain'+chain+'block'+i+'prev').val()){
+        
         $('#chain'+chain+'block'+i+'card').removeClass('bg-light').addClass('card-error');
-        if($('#chain'+chain+'block'+i+'hash').val()!=$('#chain'+chain+'block'+(i+1).toString()+'prev').val() && i!=chainSize){
-          $('#chain'+chain+'block'+i+'hash').addClass('input-error');
-          $('#chain'+chain+'block'+(i+1).toString()+'prev').addClass('input-error');
+
+        if($('#chain'+chain+'block'+(i-1).toString()+'hash').val()!=$('#chain'+chain+'block'+i+'prev').val()){  //previous and current hash DON'T match
+          $('#chain'+chain+'block'+(i-1).toString()+'card').removeClass('bg-light').addClass('card-error');
+          $('#chain'+chain+'block'+(i-1).toString()+'hash').addClass('input-error');
+          $('#chain'+chain+'block'+i+'prev').addClass('input-error');
+        }
+        else{ //previous and current hash match
+          $('#chain'+chain+'block'+(i-1).toString()+'hash').removeClass('input-error');
+          $('#chain'+chain+'block'+i+'prev').removeClass('input-error');
+        }
+
+        if($('#chain'+chain+'block'+(i-1).toString()+'magic').val()!=$('#chain'+chain+'block'+i+'magic').val()){  //magic number has an error
+          $('#chain'+chain+'block'+i+'magic').addClass('input-error');
+        }
+        else{ //magic number has NO error
+          $('#chain'+chain+'block'+i+'magic').removeClass('input-error');
+        }
+
+        if(parseInt($('#chain'+chain+'block'+(i-1).toString()+'blockid').val())!=parseInt($('#chain'+chain+'block'+i+'blockid').val())-1){  //block id has an error
+          $('#chain'+chain+'block'+i+'blockid').addClass('input-error');
+        }
+        else{ //block id has NO error
+          $('#chain'+chain+'block'+i+'blockid').removeClass('input-error');
+        }
+    }
+    else{ //no error in current block
+      if($('#chain'+chain+'block'+(i-1).toString()+'card').hasClass('card-error')){ //previous card had an error
+        $('#chain'+chain+'block'+(i-1).toString()+'hash').removeClass('input-error');
+        if(i-1==1){ //previous card was the first card in the chain
+          $('#chain'+chain+'block'+(i-1).toString()+'card').removeClass('card-error').addClass('bg-light');
+          $('#chain'+chain+'block'+i+'card').removeClass('card-error').addClass('bg-light');
+        }
+        else{ //previous card was NOT the first card in the chain
+          $('#chain'+chain+'block'+i+'card').removeClass('bg-light').addClass('card-error');
         }
       }
-      else{
+      else{ //previous card had NO errors
+        $('#chain'+chain+'block'+(i-1).toString()+'card').removeClass('card-error').addClass('bg-light');
+        $('#chain'+chain+'block'+(i-1).toString()+'hash').removeClass('input-error');
         $('#chain'+chain+'block'+i+'card').removeClass('card-error').addClass('bg-light');
-        $('#chain'+chain+'block'+i+'hash').removeClass('input-error');
-        $('#chain'+chain+'block'+(i+1).toString()+'prev').removeClass('input-error');
       }
-    }
-  }
 
-  function updateChain(chain, block, chainSize) {
-    for (var i = block; i <= chainSize; i++) {
-      if (i > 1) {
-        $('#chain'+chain+'block'+i+'prev').val($('#chain'+chain+'block'+(i-1).toString()+'hash').val());
-      }
-      updateHash(chain, i);
+      $('#chain'+chain+'block'+i+'magic').removeClass('input-error');
+      $('#chain'+chain+'block'+i+'blockid').removeClass('input-error');
+      $('#chain'+chain+'block'+i+'prev').removeClass('input-error');
     }
   }
+}
+
+function updateChain(chain, block, chainSize) {
+  for (var i = block; i <= chainSize; i++) {
+    if (i > 1) {
+      $('#chain'+chain+'block'+i+'prev').val($('#chain'+chain+'block'+(i-1).toString()+'hash').val());
+    }
+    updateHash(chain, i);
+  }
+}
